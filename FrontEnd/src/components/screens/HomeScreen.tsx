7eba47f70a1e28013527049ab680b383930d7eae
jadenho10 <jaden.ho@gmail.com>
2025-04-23 22:21:07 -0700

    Starting the front end

---
+++ FrontEnd/src/components/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+
+  import { NavigationContainer } from '@react-navigation/native';
+  import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  import { StyleSheet } from 'react-native';
+  import { SafeAreaProvider } from "react-native-safe-area-context"
+  import { Toaster } from 'sonner-native';
+  import HomeScreen from "./screens/HomeScreen"
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
